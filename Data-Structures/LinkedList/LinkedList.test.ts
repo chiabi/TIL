033697394a37ed5694e21d56a44ada264e079aae
chiabi <chiabi88@gmail.com>
2018-08-08 18:36:54 +0900

    타입스크립트 적용한 prepend, append, read 기능과 테스트 추가

--- Data-Structures/LinkedList/LinkedList.test.ts
+++ Data-Structures/LinkedList/LinkedList.test.ts
@@ -1,19 +1,53 @@
-import { LinkedListNode, LinkedList } from './LinkedList'
+import { LinkedList } from './LinkedList'
 
 // describe(name, fn): 관련된 test 그룹을 하나의 블록으로 만들어 생성한다. 
 // 이 블록은 하나의 'test suite'안에서 테스트 된다.
 describe('LinkedList', () => {
-  it('빈 연결리스트를 생성한다.', () => {
-    const linkedList = new LinkedList()
-    expect(linkedList.get()).toBe(-1)
-  })
-  it('연결리스트에 노드를 추가한다.', () => {
-    const linkedList = new LinkedList()
-    const expected = {data: 4, next: {}}
-    expect(linkedList.get(0)).toBe(-1)
+  describe('빈 연결리스트', () => {
+    it('빈 연결리스트를 생성한다.', () => {
+      const linkedList = new LinkedList()
+      const expected = {firstNode: null, lastNode: null, listLength: 0}
+      expect(linkedList.read(0)).toBeNull()
+      expect(linkedList).toEqual(expected)
+    })
+    it('빈 연결리스트 앞에 노드를 추가한다.', () => {
+      const linkedList = new LinkedList()
+      const expected = {
+        firstNode: {
+          value: 'chiabi', 
+          next: {
+            value: 2, 
+            next: {
+              value: 8,
+              next :null
+            }
+          }
+        }, 
+        lastNode: {value: 8, next: null},
+        listLength: 3
+      }
+      expect(linkedList.read(0)).toBeNull()
 
-    linkedList.addAtTail(4)
-    expect(linkedList.get(0)).toBe(4)
-    expect(linkedList).toEqual(expected)
+      linkedList.prepend(2)
+      expect(linkedList.read(0)).toBe(2)
+      linkedList.append(8)
+      linkedList.prepend('chiabi')
+      expect(linkedList.read(2)).toBe(8)
+      expect(linkedList).toEqual(expected)
+    })
+    // it('빈 연결리스트 뒤에 노드를 추가한다.', () => {
+    //   const linkedList = new LinkedList()
+    //   expect(linkedList.read(0)).toBeNull()
+
+    // })
   })
+  // it('숫자 4를 첫번째 노드로 가지는 연결리스트를 생성한다.', () => {
+  //   const linkedList = new LinkedList()
+  //   const expected = {data: 4, next: {}}
+  //   expect(linkedList.read(0)).toBeNull()
+
+  //   linkedList.append(4)
+  //   expect(linkedList.read(0)).toB(4)
+  //   expect(linkedList).toEqual(expected)
+  // })
 })
--- Data-Structures/LinkedList/LinkedList.ts
+++ Data-Structures/LinkedList/LinkedList.ts
@@ -1,15 +1,70 @@
-export class LinkedListNode {
-    data: number
-    nextNode: object | null
-    constructor(data: number) {
-        this.data = data
-        this.nextNode = null
-    }
+interface INode {
+    value: string | number;
+    next: INode | null
+}
+class LinkedListNode implements INode {
+    constructor(
+        public value: string | number, 
+        public next: INode | null = null) {}
 }
 
 export class LinkedList {
-    firstNode: object | null
-    constructor(firstNode: object | null) {
+    firstNode: INode | null
+    lastNode: INode | null
+    listLength: number
+    constructor(firstNode: INode | null = null) {
         this.firstNode = firstNode
+        this.lastNode = firstNode
+        this.listLength = !firstNode ? 0 : 1
+    }
+
+    read(index: number) {
+        let currentIndex: number = 0
+        let currentNode = this.firstNode;
+        if(index >= this.listLength) {
+            return null
+        }
+        while(currentIndex < index) {
+            currentNode = (<INode>currentNode).next;
+            currentIndex++;
+        }
+        return (<INode>currentNode).value
+    }
+
+    indexOf(value: string | number) {
+        // return 
+    }
+
+    prepend(value: string | number) {
+        const newNode: INode = new LinkedListNode(value, this.firstNode)
+        this.firstNode = newNode
+
+        if (!this.lastNode) {
+            this.lastNode = newNode
+        } 
+        this.listLength++
+        return this
+    }
+
+    append(value: string | number) {
+        const newNode: INode = new LinkedListNode(value)
+        if(!this.firstNode) {
+            this.firstNode = newNode
+            this.lastNode = newNode
+            this.listLength++
+            return this
+        } 
+        (<INode>this.lastNode).next = newNode
+        this.lastNode = newNode
+        this.listLength++
+        return this
+    }
+
+    insertAtIndex(index: number, value: string | number) {
+
+    }
+
+    deleteAtIndex(index: number) {
+
     }
 }
