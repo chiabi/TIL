3aea567c6f256f4abdf53f646a307093c63711c8
chiabi <chiabi88@gmail.com>
2018-09-21 17:23:02 +0900

    GraphQL movie API 예제 - Delete Mutation

--- GraphQL/movieql/graphql/db.js
+++ GraphQL/movieql/graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+let movies = [
   {
     id: 1,
-    name: 'syami',
-    age: 9,
-    gender: 'female'
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
     id: 2,
-    name: 'jojo',
-    age: 8,
-    gender: 'male'
+    name: "Avengers - The new one",
+    score: 8
   },
   {
     id: 3,
-    name: 'loki',
-    age: 5,
-    gender: 'male'
-  },
+    name: "The Godfather I",
+    score: 99,
+  }, 
   {
     id: 4,
-    name: 'kkonnim',
-    age: 4,
-    gender: 'female'
-  },
+    name: "Logan",
+    score: 2
+  }
 ]
 
+export const getMovies = () => movies;
+
 export const getById = id =>  {
-  const filteredPeople = people.filter(person => id === person.id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+}
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if(movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 }
--- GraphQL/movieql/graphql/resolvers.js
+++ GraphQL/movieql/graphql/resolvers.js
@@ -1,9 +1,13 @@
-import { people, getById } from './db'
+import { getMovies, getById, addMovie, deleteMovie } from './db'
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, {id}) => getById(id)
+    movies: () => getMovies(),
+    movie: (_, {id}) => getById(id)
+  },
+  Mutation: {
+    addMovie: (_, {name, score}) => addMovie(name, score),
+    deleteMovie: (_, {id}) => deleteMovie(id)
   }
 };
 
