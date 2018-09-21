446c96831cf2ba603f585149dbfbd71260a09c77
chiabi <chiabi88@gmail.com>
2018-09-21 17:52:22 +0900

    GraphQL 예제 - YTS REST API를 GraphQL로 감싸기

--- GraphQL/movieql/graphql/db.js
+++ GraphQL/movieql/graphql/db.js
@@ -1,49 +1,15 @@
-let movies = [
-  {
-    id: 1,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 2,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 3,
-    name: "The Godfather I",
-    score: 99,
-  }, 
-  {
-    id: 4,
-    name: "Logan",
-    score: 2
+import fetch from 'node-fetch';
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if(limit > 0) {
+    REQUEST_URL += `limit=${limit}&`
   }
-]
-
-export const getMovies = () => movies;
-
-export const getById = id =>  {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
-}
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if (rating > 0) {
+    REQUEST_URL += `minimum_rating=${rating}`
   }
-}
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies)
 }
--- GraphQL/movieql/graphql/resolvers.js
+++ GraphQL/movieql/graphql/resolvers.js
@@ -1,14 +1,9 @@
-import { getMovies, getById, addMovie, deleteMovie } from './db'
+import { getMovies } from './db'
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, {id}) => getById(id)
+    movies: (_, {limit, rating}) => getMovies(limit, rating),
   },
-  Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score),
-    deleteMovie: (_, {id}) => deleteMovie(id)
-  }
 };
 
 export default resolvers;
