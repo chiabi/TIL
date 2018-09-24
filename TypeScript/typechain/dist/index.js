dbdd92150c43d2efa1bd7324ed778bddc153719c
chiabi <chiabi88@gmail.com>
2018-09-24 13:10:34 +0900

    타입스크립트 예제 - Interface

--- TypeScript/typechain/dist/index.js
+++ TypeScript/typechain/dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+const person = {
+    name: "chiabi",
+    gender: "female",
+    age: 28
 };
-console.log(sayHi("chiabi", 28, "female"));
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- TypeScript/typechain/src/index.ts
+++ TypeScript/typechain/src/index.ts
@@ -1,6 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "chiabi",
+  gender: "female",
+  age: 28
 };
 
-console.log(sayHi("chiabi", 28, "female"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }!`;
+};
+
+console.log(sayHi(person));
 export {};
