679ef670f10468e814db2ff15b0813f85c5f0a81
chiabi <chiabi88@gmail.com>
2018-09-25 20:29:44 +0900

    Typescript 예제 - Block class

--- TypeScript/typechain/src/index.ts
+++ TypeScript/typechain/src/index.ts
@@ -1,21 +1,22 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = this.previousHash;
+    this.data = data;
+    this.timestamp = this.timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 28, "female");
-
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${
-    person.gender
-  }!`;
-};
-
-console.log(sayHi(lynn));
 export {};
